6dbdbd52d0168ee1864664d208bda5cfd4d01d2c
Tom Whitehead <tw330@cam.ac.uk>
2011-05-30 14:39:57 +0100

    changed DWTgrp

--- Group/DWTgrp.m
+++ Group/DWTgrp.m
@@ -1,23 +1,32 @@
 function Z = DWTgrp(X)
 
 % set the parameters
-layers = 5; k = 0.8; delta = 12;
+levels = 5; k = 0.8; delta = 12;
 
-% 
-steps = DWT_steps(layers,k)*delta;
+% form the equal MSE step sizes
+steps = DWT_steps(levels,k)*delta;
 
+% set first rise for quantisation
+rise = steps*0.5;
+
+% perform the iterative DWT
 Y = DWT_iter(X,levels);
-[Yq entropy_array total_bits] = DWT_quantise(Y,levels,steps);
+
+% quantise the image (Yq) and get entropy and bit rate information
+[Yq entropy_array total_bits] = DWT_quantise(Y,levels,steps,rise);
+
+% reform the image to return
 Z = IDWT_iter(Yq, levels);
 
 
 
-
+% uncomment to output entropy and bit rate information
 %%total_bits
 %%entropy_array
 %%avg_entropy = total_bits / 256 / 256
+%%compression_ratio = 2.2812e5 / total_bits
 
+% uncomment to output rms error
 %%error = std(X(:) - Z(:))
-
 
 end
--- Group/tw330 Supporting Files/DWT/DWT_quantise.m
+++ Group/tw330 Supporting Files/DWT/DWT_quantise.m
@@ -1,4 +1,4 @@
-function [Yq ent bits] = DWT_quantise(Y,levels,step)
+function [Yq ent bits] = DWT_quantise(Y,levels,step,rise)
 
 m = 256;
 Yq = Y;
@@ -8,21 +8,20 @@ for i = 1:levels
     m = m/2; 
     t = 1:m;
     
-    Yq(t,m+t) = quantise(Yq(t,m+t),step(i));            % top right
+    Yq(t,m+t) = quantise(Yq(t,m+t),step(i),rise(i));            % top right
     ent(1,i) = entropy(Yq(t,m+t),step(i));
     
-    Yq(m+t,t) = quantise(Yq(m+t,t),step(i));            % bottom left
+    Yq(m+t,t) = quantise(Yq(m+t,t),step(i),rise(i));            % bottom left
     ent(2,i) = entropy(Yq(m+t,t),step(i));
     
-    Yq(m+t,m+t) = quantise(Yq(m+t,m+t),step(i));        % bottom right
+    Yq(m+t,m+t) = quantise(Yq(m+t,m+t),step(i),rise(i));        % bottom right
     ent(3,i) = entropy(Yq(m+t,m+t),step(i));
     
     bits = bits + m*m*(ent(1,i)+ent(2,i)+ent(3,i));
 end
 
-
 t = 1:m;
-Yq(t,t) = quantise(Yq(t,t),step(levels+1));             % low pass bit
+Yq(t,t) = quantise(Yq(t,t),step(levels+1),rise(levels+1));             % low pass bit
 ent(1,levels+1) = entropy(Yq(t,t),step(levels+1));
 
 bits = bits + m*m*(ent(1,levels+1));
