d543ca20fe9a7df8dfafb748dcaaefe78df7913f
Tom Whitehead <tw330@cam.ac.uk>
2011-06-02 15:08:41 +0100

    scan for any size DCT

--- Group/DCT8ptbitenc.m
+++ Group/DCT8ptbitenc.m
@@ -18,13 +18,31 @@ Yq=quant1(Y,stp,rise);
 sy=size(Yq);
 
 % Generate zig-zag scan of AC coefs.
-if N==8,
-  scan=[9 2 3 10 17 25 18 11 4 5 12 19 26 33 41 34 27 20 13 6];
-  scan=[scan 7 14 21 28 35 42 49 57 50 43 36 29 22 15 8];
-  scan=[scan 16 23 30 37 44 51 58 59 52 45 38 31 24];
-  scan=[scan 32 39 46 53 60 61 54 47 40 48 55 62 63 56 64];
+scan = [];
+% scanning direction, (1 for up, 0 for down)
+dir = 0;
+for k = 2:2*N
+    dir = 1-dir;
+        
+    if dir == 1
+       for row = k:-1:1
+           col = ((k+1)-row);
+           if row <= N && col <= N,
+             scan = [scan ((row-1)*N+col)];  
+           end
+       end
+    else
+        for col = k:-1:1
+           row = ((k+1)-col);
+           if row <= N && col <= N,
+             scan = [scan ((row-1)*N+col)];
+           end
+        end
+    end
 end
 
+    
+    
 % On the first pass use default huffman tables.
 disp('Generating huffcode and ehuf using default tables.')
 [bits, huffval] = huffdflt(1);  % Default tables.
@@ -46,7 +64,7 @@ for r=0:N:(sy(1)-N),
     if ((yq(1)<-127) | (yq(1)>128))
       disp('DC coefficients too high');
     end
-    dccoef = [min(127,max(-127,yq(1)))+128  8]; 
+    dccoef = [min(127,max(-127,yq(1)))+128  8];%
     ra1 = runampl(yq(scan));
     vlc1 = [vlc1; dccoef; huffenc(ra1)]; % huffenc() uses ehuf and updates huffhist.
   end
@@ -57,12 +75,12 @@ disp('Coding done')
 fprintf(1,'No. of bits for coded image with default tables = %d\n', sum(vlc(:,2)))
 
 % Plot histogram of use of huffman codes.
-figure(1)
-bar(0:length(huffhist)-1, huffhist)
-title('Histogram of huffman code usage')
-drawnow
+%figure(1)
+%bar(0:length(huffhist)-1, huffhist)
+%title('Histogram of huffman code usage')
+%drawnow
 
-save compress vlc bits huffval stp sy N scan
+save compress vlc bits huffval stp rise sy N scan
 
 % Return here if the default tables are sufficient, otherwise repeat the
 % encoding process using the custom designed huffman tables.
@@ -90,7 +108,7 @@ for r=0:N:(sy(1)-N),
     if ((yq(1)<-127) | (yq(1)>128))
       disp('DC coefficients too high');
     end
-    dccoef = [min(127,max(-127,yq(1)))+128  8]; 
+    dccoef = [min(127,max(-127,yq(1)))+128  8];% 
     ra1 = runampl(yq(scan));
     vlc1 = [vlc1; dccoef; huffenc(ra1)]; % huffenc() uses ehuf and updates huffhist.
   end
@@ -100,5 +118,5 @@ disp('Coding done')
 
 fprintf(1,'No. of bits for coded image with custom tables = %d\n', sum(vlc(:,2)))
 
-save compress vlc bits huffval stp sy N scan
+save compress vlc bits huffval stp rise sy N scan
 
